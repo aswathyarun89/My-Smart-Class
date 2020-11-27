c627a25bced32292827c8ba592789436e5f44585
aswathyarun89 <aswathy.sa2@gmail.com>
2020-11-27 11:04:01 +0000

    first Committ

--- mydjangoproject2/settings.py
+++ mydjangoproject2/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            BASE_DIR/'myapp2/template'
             os.path.join(BASE_DIR, 'templates'),
             
         ],
